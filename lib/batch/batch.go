92e720e4b3e8ba1be93d96f2676759ad43f9ba62
Peter Vereshagin <peter@vereshagin.org>
2022-07-01 15:42:25 +0000

    feat: :rocket: init

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -8,11 +10,32 @@ type user struct {
 	ID int64
 }
 
+var start = time.Now()
+
 func getOne(id int64) user {
 	time.Sleep(time.Millisecond * 100)
+	fmt.Println(time.Since(start).Milliseconds())
 	return user{ID: id}
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	// c := make(chan int64, pool)
+	var mu sync.Mutex
+	for i := int64(0); i < n; i++ {
+		wg.Add(1)
+		// c <- i
+		go func(j int64) {
+			// j := <-c
+			item := getOne(j)
+			mu.Lock()
+			res = append(res, item)
+			// defer func() {
+			mu.Unlock()
+			// }()
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
